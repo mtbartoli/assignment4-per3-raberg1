2725761fcacaa10289bb8bfafc70c6a724cb7a1c
Maddox Bartoli <mtbartoli@cps.edu>
2025-10-30 09:56:57 -0500

    py

--- a4.py
+++ a4.py
@@ -10,8 +10,43 @@ class TTTBoard:
             represent moves by player 'O' and '*'s are spots no one has yet played on
     """
 
-    pass
+    def __init__(self):
+        """Initialize a 3x3 tic tac toe board with 9 '*'"""
+        self.board = ['*'] * 9
 
+    def __str__(self):
+        """return a string representation of the board """
+        return f"{self.board[0]} {self.board[1]} {self.board[2]}\n{self.board[3]} {self.board[4]} {self.board[5]}\n{self.board[6]} {self.board[7]} {self.board[8]}"
+
+    def make_move(self, player, pos):
+        """Places a move for the player at position pos if valid
+        
+        Args:
+            player - string "X" or "O"
+            pos - integer 0-8 representing board position
+
+        Returns:
+            True if a move was made, False otherwise
+        """
+        if 0 <= pos <= 8 and self.board[pos] == '*':
+            self.board[pos] = player
+            return True
+        return False
+    
+    def has_won(self, player):
+        """Check if the player has won
+        
+        Args:
+            player - string of either "X" or "O"
+
+        Return:
+            True if the player has won, False otherwise
+        """
+        pass
+
+    def game_over(self):
+        """Check if the game is over (someone has won or the board is full)"""
+        return self.has_won("X") or self.has_won("O") or '*' not in self.board
 
 def play_tic_tac_toe() -> None:
     """Uses your class to play TicTacToe"""
@@ -61,8 +96,11 @@ if __name__ == "__main__":
     # need to write some more tests to make sure that your TTTBoard class is behaving
     # properly.
     brd = TTTBoard()
+    print(brd)
+    print()
     brd.make_move("X", 8)
     brd.make_move("O", 7)
+    print(brd)
 
     assert brd.game_over() == False
 
